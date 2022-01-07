0b1c4620f2ed16f214f87d6c3aaee563923fe9be
EnriqueMB3 <enrique_.00@hotmail.com>
2022-01-06 21:37:52 -0600

    Updates

--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import { authReducer } from './authReducer';
-import { questionReducer } from './questionReducer';
-
-export const rootReducer = combineReducers({
-    auth: authReducer,
-    question: questionReducer
- })
